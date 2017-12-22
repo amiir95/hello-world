ae4d9c4b2fa917c50b75be7a9fd7d645100ced46
amiir95 <34550571+amiir95@users.noreply.github.com>
2017-12-22 19:31:50 +0200

    last update index

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<?php session_start();?>
 <?php include "header.php";?>
     <!-- Navigation -->
 <?php include "nav-bar.php";?>
@@ -20,7 +20,51 @@
                     Page Heading
                     <small>Secondary Text</small>
                 </h1>
+ <?php
+             $userid= $_SESSION['UserID'];
+            $query1="SELECT Fname,Lname,ProfilePic,Nickname,AuthorID,Caption,PostTime,Image,Public FROM post INNER JOIN users ON post.AuthorID=users.UserID WHERE ( Public=1 ) OR(Public=0 AND AuthorID=$userid) OR( AuthorID IN (SELECT FriendID AS ID FROM friends INNER JOIN users ON friends.FriendID =users.UserID WHERE friends.UserID=$userid UNION SELECT users.UserID AS ID FROM friends INNER JOIN users ON friends.UserID =users.UserID WHERE friends.FriendID=$userid))";
 
+            
+
+             $send_query=mysqli_query($connection,$query1);
+                      if(!$send_query)
+                       {
+                         die("ERROR!!".mysqli_error($connection));
+                       } 
+                    while($row1=mysqli_fetch_assoc($send_query))
+                    { 
+                        if(empty($row1['Nickname']))
+                       {
+                            $name=$row1['Fname']." ".$row1['Lname'];
+                             echo $row1['Caption'];
+                            echo $row1['PostTime'];
+                           
+                       }else{
+                            $name=$row1['Nickname'];
+                       
+                       
+                        echo $row1['Caption'];
+                        echo $row1['PostTime'];
+                           
+                        }
+                       
+                         $ProfilePic=$row1['ProfilePic'];
+                        
+                      $FriendID=$row1['AuthorID'];
+                     
+                    
+
+                           
+                ?>
+                
+                 <a href="profile/user_index.php?UserID=<?php echo $FriendID;?>"><img class="img-responsive img-thumbnail" width="100" height="100" src="pictures/profile-pic/<?php echo $ProfilePic?>" alt="" ></a>
+                 <a href="profile/user_index.php?UserID=<?php echo $FriendID;?>"><p class="lead"><?php echo $name?></p></a>
+
+                <hr>
+
+                <?php } 
+
+                ?>
                 <!-- First Blog Post -->
                 <h2>
                     <a href="#">Blog Post Title</a>
