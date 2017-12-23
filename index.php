a7f5445dddf5ee0dc6c9d2bc1bf90be553f1bb0a
ramyfarah <ramyfarah@users.noreply.github.com>
2017-12-23 23:40:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -17,12 +17,12 @@
             <div class="col-md-8">
 
                 <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
+                    Home
+                    
                 </h1>
  <?php
              $userid= $_SESSION['UserID'];
-            $query1="SELECT Fname,Lname,ProfilePic,Nickname,AuthorID,Caption,PostTime,Image,Public FROM post INNER JOIN users ON post.AuthorID=users.UserID WHERE ( Public=1 ) OR(Public=0 AND AuthorID=$userid) OR( AuthorID IN (SELECT FriendID AS ID FROM friends INNER JOIN users ON friends.FriendID =users.UserID WHERE friends.UserID=$userid UNION SELECT users.UserID AS ID FROM friends INNER JOIN users ON friends.UserID =users.UserID WHERE friends.FriendID=$userid))";
+            $query1="SELECT Fname,Lname,ProfilePic,Nickname,AuthorID,Caption,PostTime,Image,Public,Pid FROM post INNER JOIN users ON post.AuthorID=users.UserID WHERE ( Public=1 ) OR(Public=0 AND AuthorID=$userid) OR( AuthorID IN (SELECT FriendID AS ID FROM friends INNER JOIN users ON friends.FriendID =users.UserID WHERE friends.UserID=$userid UNION SELECT users.UserID AS ID FROM friends INNER JOIN users ON friends.UserID =users.UserID WHERE friends.FriendID=$userid)) ORDER BY PostTime DESC";
 
             
 
@@ -33,53 +33,51 @@
                        } 
                     while($row1=mysqli_fetch_assoc($send_query))
                     { 
+                        $Pid=$row1['Pid'];
+                        $Image=$row1['Image'];
                         if(empty($row1['Nickname']))
                        {
                             $name=$row1['Fname']." ".$row1['Lname'];
-                             echo $row1['Caption'];
-                            echo $row1['PostTime'];
+                            $caption= $row1['Caption'];
+                            $date= $row1['PostTime'];
                            
                        }else{
-                            $name=$row1['Nickname'];
-                       
-                       
-                        echo $row1['Caption'];
-                        echo $row1['PostTime'];
+                        $name=$row1['Nickname'];
+                        $caption= $row1['Caption'];
+                        $date= $row1['PostTime'];
                            
                         }
                        
                          $ProfilePic=$row1['ProfilePic'];
-                        
-                      $FriendID=$row1['AuthorID'];
+                         $FriendID=$row1['AuthorID'];
                      
                     
 
                            
                 ?>
                 
-                 <a href="profile/user_index.php?UserID=<?php echo $FriendID;?>"><img class="img-responsive img-thumbnail" width="100" height="100" src="pictures/profile-pic/<?php echo $ProfilePic?>" alt="" ></a>
-                 <a href="profile/user_index.php?UserID=<?php echo $FriendID;?>"><p class="lead"><?php echo $name?></p></a>
+                
+                
+                <!-- First Blog Post -->
+                <div class="row">
+                    <div class="col-xs-1">
+                        <a href="profile/user_index.php?UserID=<?php echo $FriendID;?>"><img class="img-responsive img-thumbnail" src="pictures/profile-pic/<?php echo $ProfilePic;?>" alt=""></a>
+                    </div>
+                    <div class="col-xs-3">
+                        <p class="lead"><a href="profile/user_index.php?UserID=<?php echo $FriendID;?>"><?php echo $name?></a> Posted: </p>
+                    </div>
+                </div>
+                
+                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $date; ?> </p>
+                <img class="img-responsive" style="max-height: 300px;max-width: 400px;" src="pictures/post-pic/<?php echo $Image;?>"  alt="">
+                <p> <?php echo $caption; ?>
+                </p>
+                <a class="btn btn-primary" href="post.php?Pid=<?php echo $Pid;?>">View Comments <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
-
                 <?php } 
 
                 ?>
-                <!-- First Blog Post -->
-                <h2>
-                    <a href="#">Blog Post Title</a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-                <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-                <hr>
 
                 <!-- Second Blog Post -->
                 
@@ -88,6 +86,7 @@
                 
 
                 <!-- Pager -->
+<!--
                 <ul class="pager">
                     <li class="previous">
                         <a href="#">&larr; Older</a>
@@ -96,6 +95,7 @@
                         <a href="#">Newer &rarr;</a>
                     </li>
                 </ul>
+-->
 
             </div>
 
