1dac6cd73b18e2daff9976b3a3ca98123f3c93d5
ramyfarah <ramyfarah@users.noreply.github.com>
2017-12-23 23:42:37 +0200

    Update sign_up.php

--- sign_up.php
+++ sign_up.php
@@ -1,43 +1,10 @@
-<?php include 'db.php';
-include 'header.php';
-session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
-
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
-
-    <!-- Page Content -->
-    <div class="container">
-
-        <div class="row">
-                    <!-- Side Bar start Entries Column -->
-
-                       <!-- Side Bar end Entries Column -->
-
-            <!-- Blog Entries Column -->
-            <div class="col-md-8">
-
-                
-<!--              PUT YOUR SIGN UP FORM HERE-->
-              <?php
+<?php include "header.php";?>
+<?php
+session_start();
+$_SESSION["message"]=0;
+    
 if(isset($_POST['sign_up']))
 {
 
@@ -51,13 +18,39 @@ $Phone2=$_POST['Phone2'];
 $Email=$_POST['Email'];
 $Gender=$_POST['Gender'];
 $Birthdate=$_POST['bday'];
-$PP="";
+if($_FILES['image']['size']!=0)
+{
+$PP_image = $_FILES['image']['name'];
+$PP_image_temp = $_FILES['image']['tmp_name'];
+move_uploaded_file($PP_image_temp,"pictures/profile-pic/$PP_image");
+}else
+{
+    if($Gender==1)
+    {
+        $PP_image="male.jpg";
+    }else{
+        $PP_image="female.jpg";
+    }
+    
+}
 $Hometown=$_POST['Hometown'];
 $RelationshipStatus=$_POST['Status'];;
 $Bio=$_POST['Bio'];
 
 $_SESSION["Email"] = $Email;
-$_SESSION["message"]=0;
+$_SESSION['Fname'] = $Fname;
+$_SESSION['Lname'] = $Lname;
+$_SESSION['Nickname'] = $Nickname;
+$_SESSION['ProfilePic'] = $PP_image;
+$_SESSION['Phone1'] = $Phone1;
+$_SESSION['Phone2'] = $Phone2;
+$_SESSION['Gender'] = $Gender;
+$_SESSION['Birthdate'] = $Birthdate;
+$_SESSION['Hometown'] = $Hometown;
+$_SESSION['RelationshipStatus'] = $RelationshipStatus;
+$_SESSION['Bio'] = $Bio;
+    $_SESSION['Request_num']=0;
+
 //$_SESSION["message"]=1;
 
 
@@ -70,12 +63,19 @@ $_SESSION["message"] = 1;
 }
 else 
 {
-    $Password2 = password_hash($password,PASSWORD_BCRYPT,array('cost'=>12));
-$sql = "INSERT into Users(Fname,Lname,Nickname,Password,Phone1,Phone2,Email,Gender,Birthdate,ProfilePic,Hometown,RelationshipStatus,Bio) values ('$Fname','$Lname','$Nickname','$Password2','$Phone1','$Phone2','$Email','$Gender','$Birthdate','$PP','$Hometown','$RelationshipStatus','$Bio')";
+$Password2 = password_hash($Password,PASSWORD_BCRYPT,array('cost'=>12));
+$sql = "INSERT into Users(Fname,Lname,Nickname,Password,Phone1,Phone2,Email,Gender,Birthdate,ProfilePic,Hometown,RelationshipStatus,Bio) values ('{$Fname}','{$Lname}','{$Nickname}','{$Password2}','{$Phone1}','{$Phone2}','{$Email}','{$Gender}','{$Birthdate}','{$PP_image}','{$Hometown}','{$RelationshipStatus}','{$Bio}')";
 $search = mysqli_query($connection,$sql);
-header("Location: test.php");
+$UserID=mysqli_insert_id($connection);
+if(!$search)
+{
+    die("ERROR".mysqli_error($connection));
+}else{
+$_SESSION["UserID"] = $UserID;
+header("Location: profile/p_index.php");
 }
-$connection->close();
+
+}
 }
 ?>
 
@@ -123,28 +123,63 @@ $connection->close();
                     else return true;
                 }
                 </script>
-    </head>
-    <body>
-        <div> <h3>SignUp</h3> </div>
-            <form id="form1" name="form1" action="" method="post" onsubmit="return signUpForm()">
+
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+                    <!-- Side Bar start Entries Column -->
+
+                       <!-- Side Bar end Entries Column -->
+
+            <!-- Blog Entries Column -->
+            <div class="col-md-8 well">
+
+                
+<!--              PUT YOUR SIGN UP FORM HERE-->
+              <h3>SignUp</h3> 
+            <form id="form1" name="form1" action="" method="post" onsubmit="return signUpForm()" enctype="multipart/form-data">
                 <h3>*First Name (Required)</h3>
-                <input type = "text" Name ="Fname" ID="Fname"  placeholder="First Name" >
+                <input class="form-control" type = "text" Name ="Fname" ID="Fname"  placeholder="First Name" >
                 <br />
                 <h3>*Last Name Required)</h3>
-                <input type = "text" Name ="Lname" ID="Lname" placeholder="Last Name" >
+                <input class="form-control" type = "text" Name ="Lname" ID="Lname" placeholder="Last Name" >
                 <br />
                 <h3>Nickname :</h3>
-                <input type = "text" Name ="Nickname" ID="Nickname" placeholder="Nickname" >
+                <input class="form-control" type = "text" Name ="Nickname" ID="Nickname" placeholder="Nickname" >
                 <br />
                 <h3>*Password (Required)</h3>
-                <input type = "password" Name ="password" ID="password" placeholder="password min:8" >
+                <input class="form-control" type = "password" Name ="password" ID="password" placeholder="password min:8" >
                 <br />
+                <h3>*Profile Picture</h3>
+                <input name="image" type="file">
+                <br>
                 <h3>Phone(s) </h3>
-                <input type = "number" Name ="Phone1" ID="Phone1" placeholder="Phone(1)ex:01XXXXXXXXX" >
-                <input type = "number" Name ="Phone2" ID="Phone2" placeholder="Phone(2)ex:01XXXXXXXXX" >
+                <input class="form-control" type = "text" Name ="Phone1" ID="Phone1" placeholder="Phone(1)ex:01XXXXXXXXX" >
+                <input class="form-control" type = "text" Name ="Phone2" ID="Phone2" placeholder="Phone(2)ex:01XXXXXXXXX" >
                 <br />
                 <h3>*Email (Required)</h3>
-                <input type = "text" Name ="Email" ID="Email" placeholder="E-mail e.g: asdfgh@qwert.ds" >
+                <input class="form-control" type = "text" Name ="Email" ID="Email" placeholder="E-mail e.g: asdfgh@qwert.ds" >
                 
                 <?php
                 if($_SESSION["message"] == 1){
@@ -155,27 +190,25 @@ $connection->close();
 
                 <br />
                 <h3>*Gender (Required)</h3>
-                <input type="radio" name="Gender" ID="Male" value="0" > Male
-                <input type="radio" name="Gender" ID="Female" value="1"> Female<br>
+                <input type="radio" name="Gender" ID="Male" value="1" > Male
+                <input type="radio" name="Gender" ID="Female" value="0"> Female<br>
                 <br />
                 <h3>*Birthdate (Required)</h3>
-                <input type="date" name="bday" required="true">
+                <input class="form-control" type="date" name="bday" required="true">
                 <br />
                 <h3>Hometown:</h3>
-                <input type = "text" Name ="Hometown" ID="Hometown"  placeholder="Hometown" >
+                <input class="form-control" type = "text" Name ="Hometown" ID="Hometown"  placeholder="Hometown" >
                 <h3>*Marital status:</h3>
-                <input type="radio" name="Status" ID="Single" value="Single" > Single
-                <input type="radio" name="Status" ID="Enagaged" value="Engaged"> Engaged
-                <input type="radio" name="Status" ID="Married" value="Married"> Married
+                <input  type="radio" name="Status" ID="Single" value="Single" > Single
+                <input  type="radio" name="Status" ID="Enagaged" value="Engaged"> Engaged
+                <input  type="radio" name="Status" ID="Married" value="Married"> Married
                 <br />
                 <h3>About me:</h3>
-                <input type = "text" Name ="Bio" ID="Bio"  placeholder="bio.." >
+                <input class="form-control" type = "text" Name ="Bio" ID="Bio"  placeholder="bio.." >
                 <br />
                 <br />
-                <input type ="submit" Name="sign_up" value = "SIGN UP">
+                <input class="form-control btn btn-primary" type ="submit" Name="sign_up" value = "SIGN UP">
             </form>
-    </body>
-</html>
 
             </div>
 
